ef6a9cf7906d6419ad0d3aa1cec59a4e5cc19362
haruya3 <haruya.nagai.3y@stu.hosei.ac.jp>
2021-08-31 23:28:57 +0900

    Postモデル作成

---
+++ app/models/post.rb
@@ -0,0 +1,10 @@
+class Post < ApplicationRecord
+    validates :user_id, {presence: true}
+    validates :title, {presence: true, length:{maximum: 20}}
+    validates :content, {presence: true, length:{maximum: 500}}
+
+    def chnge_to_user
+        ##PostモデルのインスタンスからUserモデルのインスタンスへ変換、使うかな？？
+        ##@user = User.find_by(id: self.user_id)
+    end
+end
