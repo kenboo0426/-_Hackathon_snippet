1c03f0a4aa36a16afb2e921f89ad8865b30e26ee
kenboo0426 <kenshin.kenboo@gmail.com>
2021-09-04 17:37:49 +0900

    session_controller バグ修正

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::SessionsController < ApplicationController
-  
+
   SECRET_KEY = Rails.application.secrets.secret_key_base
 
   def create
@@ -13,15 +13,4 @@ class Api::V1::SessionsController < ApplicationController
     end
   end
 
-  def create_tokens
-    expires_in = 1.month.from_now.to_i # 再ログインを必要とするまでの期間を１ヶ月とした場合
-    payload = { user_id: id }
-    issue_token(payload.merge(exp: expires_in))
-  end
-
-  private
-
-    def issue_token(payload)
-      JWT.encode(payload, SECRET_KEY, 'HS256')
-    end
 end
