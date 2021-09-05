687225050d63063cbb0ce768ddb9c90e8c4ea5cd
kenboo0426 <kenshin.kenboo@gmail.com>
2021-09-05 11:57:43 +0900

    commentモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_04_064006) do
+ActiveRecord::Schema.define(version: 2021_09_05_025222) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "comment"
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "htags", force: :cascade do |t|
     t.string "name"
@@ -52,6 +62,8 @@ ActiveRecord::Schema.define(version: 2021_09_04_064006) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "post_tags", "posts", on_delete: :cascade
   add_foreign_key "post_tags", "tags", on_delete: :cascade
   add_foreign_key "posts", "users"
